9cedccc1bb31fb052232de6ffb8474eba5f64c6c
josip-volarevic <josip.volarevic4@gmail.com>
2023-01-19 01:29:55 +0100

    chore: update candy machine service

--- src/vendors/candy-machine.service.ts
+++ src/vendors/candy-machine.service.ts
@@ -7,7 +7,6 @@ import {
 } from '@solana/web3.js';
 import { PrismaService } from 'nestjs-prisma';
 import {
-  IdentitySigner,
   keypairIdentity,
   Metaplex,
   MetaplexFile,
@@ -18,13 +17,14 @@ import {
 } from '@metaplex-foundation/js';
 import * as AES from 'crypto-js/aes';
 import * as Utf8 from 'crypto-js/enc-utf8';
-import { readFileSync } from 'fs';
 import { awsStorage } from '@metaplex-foundation/js-plugin-aws';
 import { getS3Object, s3Client } from 'src/aws/s3client';
 import { Comic, ComicIssue, Creator } from '@prisma/client';
 import { Readable } from 'stream';
 import * as bs58 from 'bs58';
 import * as path from 'path';
+import { chunk } from 'lodash';
+import { sleep } from 'src/utils/helpers';
 
 const streamToString = (stream: Readable) => {
   return new Promise<Buffer>((resolve, reject) => {
@@ -72,6 +72,8 @@ const CREATOR_ARRAY_START =
 const D_READER_SYMBOL = 'dReader';
 const D_PUBLISHER_SYMBOL = 'dPublisher';
 
+const D_READER_FRONTEND_URL = 'https://dreader.app';
+
 const SECONDARY_SALE_TAX = 800; // 8%
 const HUNDRED = 100;
 const HUNDRED_PERCENT_TAX = 10000;
@@ -111,15 +113,14 @@ export class CandyMachineService {
 
     this.metaplex
       .use(keypairIdentity(treasuryKeypair))
+      // TODO: add this bucket name to .env so local development doesn't mess up prod
       .use(awsStorage(s3Client, 'd-reader-nft-data'));
   }
 
-  async findMintedNfts() {
+  async findMintedNfts(candyMachineAddress: string) {
     try {
-      const candyMachineId = new PublicKey(
-        '9Z76zEyYT1GS6pMcUP4TLVQcLWiySyURKsgw9cRnQakn',
-      );
-      const candyMachineCreator = await PublicKey.findProgramAddress(
+      const candyMachineId = new PublicKey(candyMachineAddress);
+      const candyMachineCreator = PublicKey.findProgramAddressSync(
         [Buffer.from('candy_machine'), candyMachineId.toBuffer()],
         this.metaplex.programs().getCandyMachine().address,
       );
@@ -168,6 +169,12 @@ export class CandyMachineService {
     comicIssue: ComicIssue,
     creator: Creator,
   ) {
+    if (comicIssue.supply <= 0) {
+      throw new BadRequestException(
+        'Cannot create an NFT collection with supply lower than 1',
+      );
+    }
+    // TODO copy S3 image from one bucket to another (with d-reader-nft-data)
     const coverImage = await this.generateMetaplexFileFromS3(comicIssue.cover);
 
     const collectionNft = await this.createComicIssueCollectionNft(
@@ -175,92 +182,98 @@ export class CandyMachineService {
       coverImage,
     );
 
-    try {
-      const comicCreator = new PublicKey(creator.walletAddress);
+    // v2: try catch
+    const comicCreator = new PublicKey(creator.walletAddress);
 
-      const { candyMachine } = await this.metaplex.candyMachines().create(
-        {
-          candyMachine: Keypair.generate(), // v2: prefix key with 'cndy'
-          authority: this.metaplex.identity(),
-          collection: {
-            address: collectionNft.address,
-            updateAuthority: this.metaplex.identity(),
-          },
-          symbol: D_PUBLISHER_SYMBOL,
-          maxEditionSupply: toBigNumber(0),
-          isMutable: true,
-          sellerFeeBasisPoints: SECONDARY_SALE_TAX,
-          itemsAvailable: toBigNumber(comicIssue.supply),
-          // groups: [], // v2: add different groups
-          guards: {
-            botTax: { lamports: sol(GLOBAL_BOT_TAX), lastInstruction: false },
-            solPayment: {
-              amount: sol(comicIssue.mintPrice),
-              destination: this.metaplex.identity().publicKey,
-            },
-            tokenPayment: undefined,
-            startDate: { date: toDateTime(comicIssue.releaseDate) },
-            endDate: undefined, // v2: close mints after a long period of stale sales?
-            thirdPartySigner: { signerKey: this.metaplex.identity().publicKey }, // v2: do we really need this?
-            tokenGate: undefined, // v2: gate minting to $PAGES non-holders
-            gatekeeper: undefined, // v2: add bot protection
-            allowList: undefined, // v2: make sure that holders of previous issues have advantage of miting ahead of time
-            mintLimit: undefined, // v2: possibly limit minting to ~2 NFTs or depending on your 'level'
-            redeemedAmount: undefined, // v2: possibly secure n% of the supply for dReader drops (staking) and creators wallet
-            addressGate: undefined, // v2: combine with groups and redemeedAmount to secure mints for creators and dReader
-            nftPayment: undefined,
-            nftGate: undefined, // v2: gate special editions in the future?
-            nftBurn: undefined, // v2: creators can drop 'FREE MINT' NFTs to end users
-            tokenBurn: undefined,
-            freezeSolPayment: undefined,
-            freezeTokenPayment: undefined,
-            programGate: undefined,
-          },
-          creators: [
-            {
-              address: this.metaplex.identity().publicKey,
-              share: D_PUBLISHER_PRIMARY_SALE_SHARE,
-            },
-            {
-              address: comicCreator,
-              share: HUNDRED - D_PUBLISHER_PRIMARY_SALE_SHARE,
-            },
-          ],
+    const { candyMachine } = await this.metaplex.candyMachines().create(
+      {
+        candyMachine: Keypair.generate(), // v2: prefix key with 'cndy'
+        authority: this.metaplex.identity(),
+        collection: {
+          address: collectionNft.address,
+          updateAuthority: this.metaplex.identity(),
         },
-        { payer: this.metaplex.identity() }, // v2: in the future comicCreator might become the payer
+        symbol: D_PUBLISHER_SYMBOL,
+        maxEditionSupply: toBigNumber(0),
+        isMutable: true,
+        sellerFeeBasisPoints: SECONDARY_SALE_TAX,
+        itemsAvailable: toBigNumber(comicIssue.supply),
+        // groups: [], // v2: add different groups
+        guards: {
+          botTax: { lamports: sol(GLOBAL_BOT_TAX), lastInstruction: false },
+          solPayment: {
+            amount: sol(comicIssue.mintPrice),
+            destination: this.metaplex.identity().publicKey,
+          },
+          tokenPayment: undefined,
+          startDate: { date: toDateTime(comicIssue.releaseDate) },
+          endDate: undefined, // v2: close mints after a long period of stale sales?
+          thirdPartySigner: { signerKey: this.metaplex.identity().publicKey }, // v2: do we really need this?
+          tokenGate: undefined, // v2: gate minting to $PAGES non-holders
+          gatekeeper: undefined, // v2: add bot protection
+          allowList: undefined, // v2: make sure that holders of previous issues have advantage of miting ahead of time
+          mintLimit: undefined, // v2: possibly limit minting to ~2 NFTs or depending on your 'level'
+          redeemedAmount: undefined, // v2: possibly secure n% of the supply for dReader drops (staking) and creators wallet
+          addressGate: undefined, // v2: combine with groups and redemeedAmount to secure mints for creators and dReader
+          nftPayment: undefined,
+          nftGate: undefined, // v2: gate special editions in the future?
+          nftBurn: undefined, // v2: creators can drop 'FREE MINT' NFTs to end users
+          tokenBurn: undefined,
+          freezeSolPayment: undefined,
+          freezeTokenPayment: undefined,
+          programGate: undefined,
+        },
+        creators: [
+          {
+            address: this.metaplex.identity().publicKey,
+            share: D_PUBLISHER_PRIMARY_SALE_SHARE,
+          },
+          {
+            address: comicCreator,
+            share: HUNDRED - D_PUBLISHER_PRIMARY_SALE_SHARE,
+          },
+        ],
+      },
+      { payer: this.metaplex.identity() }, // v2: in the future comicCreator might become the payer
+    );
+
+    const sharedMetadata = await this.createComicIssueMintNftMetadata(
+      comic,
+      comicIssue,
+      creator,
+      coverImage,
+    );
+
+    const indexArray = Array.from(Array(comicIssue.supply).keys());
+    const items = await Promise.all(
+      indexArray.map((index) => ({
+        uri: sharedMetadata.uri,
+        // TODO: test if this is actually indexing or if it's done automatically
+        name: `${sharedMetadata.name} #${index + 1}`,
+      })),
+    );
+
+    const INSERT_CHUNK_SIZE = 10;
+    const itemChunksOfTen = chunk(items, INSERT_CHUNK_SIZE);
+    let iteration = 0;
+    // TODO: this most likely can't get executed in parallel but we can possibly take up more than 10 items per chunk
+    for (const itemsChunk of itemChunksOfTen) {
+      console.log(
+        `Inserting items ${iteration * INSERT_CHUNK_SIZE}-${
+          (iteration + 1) * INSERT_CHUNK_SIZE - 1
+        } `,
       );
 
-      const sharedMetadata = await this.createComicIssueSharedNftMetadata(
-        comic,
-        comicIssue,
-        creator,
-        coverImage,
-      );
-
-      // TODO: Use lookup tables to be able to properly create 10 or more items for the candy machine
-      // https://docs.solana.com/es/proposals/transactions-v2
-      // ctrl+f 'transaction too large' in the metaplex discord for more details
-      const indexArray = Array.from(Array(comicIssue.supply).keys());
-      const items = await Promise.all(
-        indexArray.map((index) => ({
-          uri: sharedMetadata.uri,
-          // TODO: test if this is actually indexing or if it's done automatically
-          name: `${sharedMetadata.name} #${index + 1}`,
-        })),
-      );
-
+      await sleep(100); // to prevent API rate limiting on free RPC nodes, we can remove this in production
       await this.metaplex.candyMachines().insertItems({
         candyMachine: candyMachine,
-        items,
+        index: iteration * INSERT_CHUNK_SIZE,
+        items: itemsChunk,
       });
-
-      return await this.metaplex.candyMachines().refresh(candyMachine);
-    } catch (e) {
-      throw new BadRequestException(
-        `Error while trying to create the CandyMachine: ${e}`,
-        e,
-      );
+      iteration = iteration + 1;
     }
+
+    return await this.metaplex.candyMachines().refresh(candyMachine);
   }
 
   // TODO: contain data about the collection: flavorText, comic name, creator name, pagesCount, issue.number...
@@ -277,7 +290,7 @@ export class CandyMachineService {
           description: comicIssue.description,
           seller_fee_basis_points: HUNDRED_PERCENT_TAX,
           image: coverImage,
-          // external_url: undefined, // v2: point to https://dreader.app/comic-issues/{comicIssue.id}
+          external_url: D_READER_FRONTEND_URL,
           properties: {
             creators: [
               {
@@ -310,7 +323,7 @@ export class CandyMachineService {
     }
   }
 
-  async createComicIssueSharedNftMetadata(
+  async createComicIssueMintNftMetadata(
     comic: Comic,
     comicIssue: ComicIssue,
     creator: Creator,
@@ -323,7 +336,7 @@ export class CandyMachineService {
         description: comicIssue.description,
         seller_fee_basis_points: SECONDARY_SALE_TAX,
         image: coverImage,
-        // external_url: undefined, // v2: point to https://dreader.app/comic-issues/{comicIssue.id}
+        external_url: D_READER_FRONTEND_URL,
         attributes: [
           {
             trait_type: STATE_TRAIT,
@@ -362,14 +375,13 @@ export class CandyMachineService {
     } catch (e) {
       throw new BadRequestException(
         `Error while trying to create the NFT metadata: ${e}`,
-        e,
       );
     }
   }
 
-  async mintOne() {
+  async mintOne(candyMachineAddress: string) {
     const candyMachine = await this.metaplex.candyMachines().findByAddress({
-      address: new PublicKey('9Z76zEyYT1GS6pMcUP4TLVQcLWiySyURKsgw9cRnQakn'),
+      address: new PublicKey(candyMachineAddress),
     });
 
     const mintNftResponse = await this.metaplex.candyMachines().mint({
@@ -381,87 +393,43 @@ export class CandyMachineService {
     });
 
     console.log('********** NFT Minted **********');
-    console.log(mintNftResponse.tokenAddress);
+    console.log(mintNftResponse.nft.address);
   }
 
-  // TODO: needs to be fixed like the function below
-  async createMintTransaction(
+  async constructMintOneTransaction(
     feePayer: PublicKey,
+    candyMachineAddress: string,
     blockhash?: BlockhashWithExpiryBlockHeight,
   ) {
     const candyMachine = await this.metaplex.candyMachines().findByAddress({
-      address: new PublicKey('8AmS1kC56CjC5ANeH9iMSUU1zrqVhTEZGypiFge9VPwY'),
+      address: new PublicKey(candyMachineAddress),
     });
 
+    const mintKeypair = Keypair.generate();
+    const dummySeller = Keypair.generate();
     const mintTransactionBuilder = await this.metaplex
       .candyMachines()
       .builders()
       .mint(
         {
+          mint: mintKeypair,
           candyMachine,
+          guards: {
+            thirdPartySigner: { signer: this.metaplex.identity() },
+          },
           collectionUpdateAuthority: this.metaplex.identity().publicKey,
         },
-        // {
-        //   payer: feePayer,
-        // },
+        { payer: dummySeller },
       );
 
     if (!blockhash) blockhash = await this.connection.getLatestBlockhash();
+    mintTransactionBuilder.setFeePayer(dummySeller);
     const mintTransaction = mintTransactionBuilder.toTransaction(blockhash);
+    mintTransaction.partialSign(mintKeypair);
 
-    mintTransaction.partialSign(this.metaplex.identity());
-    console.log('********** NFT Mint Instruction created **********');
-
-    // return bs58.encode(mintTransaction.serialize());
-
-    return bs58.encode(
-      mintTransaction.serialize({
-        requireAllSignatures: false,
-        verifySignatures: false,
-      }),
-    );
-  }
-
-  async createNftTransaction(
-    tokenOwner: PublicKey,
-    blockhash?: BlockhashWithExpiryBlockHeight,
-  ) {
-    const imageFileBuffer = readFileSync(
-      process.cwd() + '/src/vendors/logo.webp',
-    );
-
-    const { uri } = await this.metaplex.nfts().uploadMetadata({
-      name: 'Temp NFT',
-      symbol: 'dReader Test',
-      description: 'This NFT was created for PoC purposes',
-      seller_fee_basis_points: 200,
-      image: toMetaplexFile(imageFileBuffer, 'image.jpg'),
-    });
-
-    const mintKeypair = Keypair.generate();
-    const createNftBuilder = await this.metaplex
-      .nfts()
-      .builders()
-      .create(
-        {
-          uri,
-          name: 'Temp NFT',
-          sellerFeeBasisPoints: 200,
-          useNewMint: mintKeypair,
-          tokenExists: false,
-          tokenOwner,
-        },
-        { payer: { publicKey: tokenOwner } as IdentitySigner },
-      );
-
-    if (!blockhash) blockhash = await this.connection.getLatestBlockhash();
-    createNftBuilder.setFeePayer({ publicKey: tokenOwner } as IdentitySigner);
-    const createNftTransaction = createNftBuilder.toTransaction(blockhash);
-    createNftTransaction.partialSign(this.metaplex.identity());
-    createNftTransaction.partialSign(mintKeypair);
-
-    const rawTransaction = createNftTransaction.serialize({
+    const rawTransaction = mintTransaction.serialize({
       requireAllSignatures: false,
+      verifySignatures: false,
     });
 
     // return bs58.encode(rawTransaction);
