b03da199d4c845069a75c10119293e1dba9f3f8f
josip-volarevic <josip.volarevic4@gmail.com>
2023-01-31 10:49:00 +0100

    feat: Helius integration WIP

--- src/playground/playground.module.ts
+++ src/playground/playground.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuctionHouseService } from 'src/vendors/auction-house.service';
 import { CandyMachineService } from 'src/vendors/candy-machine.service';
+import { HeliusService } from 'src/vendors/helius.service';
 import { MetaplexService } from 'src/vendors/metaplex.service';
 import { PlaygroundController } from './playground.controller';
 
 @Module({
   controllers: [PlaygroundController],
-  providers: [MetaplexService, CandyMachineService, AuctionHouseService],
+  providers: [
+    MetaplexService,
+    CandyMachineService,
+    AuctionHouseService,
+    HeliusService,
+  ],
 })
 export class PlaygroundModule {}
