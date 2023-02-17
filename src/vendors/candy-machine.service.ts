a8267813e3044fed218e678b93a03fe259b8e478
Athar <athar.mohammad@dreader.io>
2023-02-18 00:25:45 +0530

    mint-one api

--- src/vendors/candy-machine.service.ts
+++ src/vendors/candy-machine.service.ts
@@ -1,10 +1,10 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import {
-  BlockhashWithExpiryBlockHeight,
   Cluster,
   Connection,
   Keypair,
   PublicKey,
+  Transaction,
 } from '@solana/web3.js';
 import { PrismaService } from 'nestjs-prisma';
 import {
@@ -12,7 +12,6 @@ import {
   Metaplex,
   MetaplexFile,
   toBigNumber,
-  toDateTime,
   toMetaplexFile,
   sol,
 } from '@metaplex-foundation/js';
@@ -28,6 +27,7 @@ import { sleep } from 'src/utils/helpers';
 import { clusterHeliusApiUrl } from 'src/utils/helius';
 import { streamToString } from 'src/utils/files';
 import { Readable } from 'stream';
+import { mintInstruction } from './instructions/mint';
 
 const MAX_NAME_LENGTH = 32;
 const MAX_URI_LENGTH = 200;
@@ -75,7 +75,8 @@ const HUNDRED_PERCENT_TAX = 10000;
 const D_PUBLISHER_PRIMARY_SALE_SHARE = 10;
 const D_PUBLISHER_SECONDARY_SALE_SHARE = 25;
 
-const GLOBAL_BOT_TAX = 0.01;
+/** @deprecated */
+// const GLOBAL_BOT_TAX = 0.01;
 
 const STATE_TRAIT = 'state';
 const SIGNED_TRAIT = 'signed';
@@ -210,13 +211,13 @@ export class CandyMachineService {
         itemsAvailable: toBigNumber(comicIssue.supply),
         // groups: [], // v2: add different groups
         guards: {
-          botTax: { lamports: sol(GLOBAL_BOT_TAX), lastInstruction: false },
+          botTax: undefined,
           solPayment: {
             amount: sol(comicIssue.mintPrice),
             destination: this.metaplex.identity().publicKey,
           },
           tokenPayment: undefined,
-          startDate: { date: toDateTime(comicIssue.releaseDate) },
+          startDate: undefined,
           endDate: undefined, // v2: close mints after a long period of stale sales?
           // thirdPartySigner: { signerKey: this.metaplex.identity().publicKey }, // v2: do we really need this?
           tokenGate: undefined, // v2: gate minting to $PAGES non-holders
@@ -408,36 +409,33 @@ export class CandyMachineService {
 
   async constructMintOneTransaction(
     feePayer: PublicKey,
-    blockhash?: BlockhashWithExpiryBlockHeight,
+    candy_machine_address: string,
   ) {
-    const candyMachine = await this.metaplex.candyMachines().findByAddress({
-      address: new PublicKey('DsSDatrYovGaMu9V1T3g9ENkTWK7okpQYHjuzy1541SM'),
-    });
-
-    const mintKeypair = Keypair.generate();
-    const dummyKeypair = Keypair.generate();
-    const mintTransactionBuilder = await this.metaplex
-      .candyMachines()
-      .builders()
-      .mint(
+    const mint = Keypair.generate();
+    const candyMachine = new PublicKey(candy_machine_address);
+    const mint_ix = await mintInstruction(
+      this.metaplex,
+      candyMachine,
+      feePayer,
+      mint,
+      this.metaplex.connection,
+      [
         {
-          mint: mintKeypair,
-          candyMachine,
-          guards: {
-            thirdPartySigner: { signer: this.metaplex.identity() },
-          },
-          collectionUpdateAuthority: this.metaplex.identity().publicKey,
-          owner:feePayer,
+          pubkey: this.metaplex.identity().publicKey,
+          isSigner: false,
+          isWritable: true,
         },
-      );
-    const ins = mintTransactionBuilder.getInstructions();
-    if (!blockhash) blockhash = await this.connection.getLatestBlockhash();
-    const mintTransaction = mintTransactionBuilder.toTransaction(blockhash);
-    mintTransaction.feePayer = feePayer;
-    mintTransaction.partialSign(mintKeypair);
-    mintTransaction.partialSign(this.metaplex.identity());
+      ],
+    );
 
-    const rawTransaction = mintTransaction.serialize({
+    const tx = new Transaction().add(...mint_ix.instructions);
+    tx.feePayer = feePayer;
+    tx.recentBlockhash = (
+      await this.metaplex.connection.getLatestBlockhash()
+    ).blockhash;
+    tx.sign(mint);
+
+    const rawTransaction = tx.serialize({
       requireAllSignatures: false,
       verifySignatures: false,
     });
--- src/vendors/instructions/mint.ts
+++ src/vendors/instructions/mint.ts
@@ -15,15 +15,20 @@ import {
   createMintInstruction,
   MintInstructionAccounts,
   MintInstructionArgs,
-} from "@metaplex-foundation/mpl-candy-guard"
+} from '@metaplex-foundation/mpl-candy-guard';
 
+import { CandyMachine, Metaplex } from '@metaplex-foundation/js';
 import {
-  CandyMachine,
-  Metaplex,
-} from '@metaplex-foundation/js';
-import { createAssociatedTokenAccountInstruction, createInitializeMintInstruction, createMintToInstruction, MintLayout, TOKEN_PROGRAM_ID } from '@solana/spl-token';
+  createAssociatedTokenAccountInstruction,
+  createInitializeMintInstruction,
+  createMintToInstruction,
+  MintLayout,
+  TOKEN_PROGRAM_ID,
+} from '@solana/spl-token';
 
-export const METAPLEX_PROGRAM_ID = new PublicKey('metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s');
+export const METAPLEX_PROGRAM_ID = new PublicKey(
+  'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s',
+);
 
 export async function mintInstruction(
   metaplex: Metaplex,
@@ -39,9 +44,9 @@ export async function mintInstruction(
   const candyMachineObject: CandyMachine = await metaplex
     .candyMachines()
     .findByAddress({
-      address: candyMachine
+      address: candyMachine,
     });
-    
+
   // PDAs.
   const authorityPda = metaplex.candyMachines().pdas().authority({
     candyMachine: candyMachine,
@@ -53,8 +58,11 @@ export async function mintInstruction(
   const nftMasterEdition = metaplex.nfts().pdas().masterEdition({
     mint: mint.publicKey,
   });
-  
-  const nftTokenAccount = metaplex.tokens().pdas().associatedTokenAccount({mint : mint.publicKey, owner : payer})
+
+  const nftTokenAccount = metaplex
+    .tokens()
+    .pdas()
+    .associatedTokenAccount({ mint: mint.publicKey, owner: payer });
 
   // collection PDAs
   const collectionMetadata = metaplex.nfts().pdas().metadata({
@@ -70,17 +78,19 @@ export async function mintInstruction(
     .collectionAuthorityRecord({
       mint: candyMachineObject.collectionMintAddress,
       collectionAuthority: authorityPda,
-  });
+    });
 
   const collectionMint = candyMachineObject.collectionMintAddress;
   // Retrieves the collection nft
-  const collection = await metaplex.nfts().findByMint({ mintAddress: collectionMint });
-  if(!candyMachineObject.candyGuard){
-      console.error("no associated candyguard !");
-      return;
+  const collection = await metaplex
+    .nfts()
+    .findByMint({ mintAddress: collectionMint });
+  if (!candyMachineObject.candyGuard) {
+    console.error('no associated candyguard !');
+    return;
   }
   const accounts: MintInstructionAccounts = {
-    candyGuard : candyMachineObject.candyGuard?.address,
+    candyGuard: candyMachineObject.candyGuard?.address,
     candyMachineProgram: CANDY_MACHINE_PROGRAM_ID,
     candyMachine,
     payer: payer,
@@ -102,42 +112,46 @@ export async function mintInstruction(
   };
 
   if (!mintArgs) {
-      mintArgs = new Uint8Array();
+    mintArgs = new Uint8Array();
   }
 
   const args: MintInstructionArgs = {
-      mintArgs,
-      label: label ?? null,
+    mintArgs,
+    label: label ?? null,
   };
 
   const ixs: TransactionInstruction[] = [];
   ixs.push(
-      SystemProgram.createAccount({
-          fromPubkey: payer,
-          newAccountPubkey: mint.publicKey,
-          lamports: await connection.getMinimumBalanceForRentExemption(MintLayout.span),
-          space: MintLayout.span,
-          programId: TOKEN_PROGRAM_ID,
-      }),
+    SystemProgram.createAccount({
+      fromPubkey: payer,
+      newAccountPubkey: mint.publicKey,
+      lamports: await connection.getMinimumBalanceForRentExemption(
+        MintLayout.span,
+      ),
+      space: MintLayout.span,
+      programId: TOKEN_PROGRAM_ID,
+    }),
   );
-  
+
   ixs.push(createInitializeMintInstruction(mint.publicKey, 0, payer, payer));
 
   ixs.push(
-      createAssociatedTokenAccountInstruction(
-          payer,
-          nftTokenAccount,
-          payer,
-          mint.publicKey,
-      ),
+    createAssociatedTokenAccountInstruction(
+      payer,
+      nftTokenAccount,
+      payer,
+      mint.publicKey,
+    ),
   );
 
-  ixs.push(createMintToInstruction(mint.publicKey, nftTokenAccount, payer, 1, []));
+  ixs.push(
+    createMintToInstruction(mint.publicKey, nftTokenAccount, payer, 1, []),
+  );
 
   const mintIx = createMintInstruction(accounts, args);
 
   if (remainingAccounts) {
-      mintIx.keys.push(...remainingAccounts);
+    mintIx.keys.push(...remainingAccounts);
   }
 
   ixs.push(mintIx);
