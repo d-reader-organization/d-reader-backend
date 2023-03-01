59852ed8a535bf11786d8d230b16e07b7c0455c3
atharmohammad <mohd.rule123@gmail.com>
2023-03-01 14:09:54 +0530

    add CM module

---
+++ src/candy-machine/candy-machine.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { CandyMachineController } from './candy-machine.controller';
+import { CandyMachineService } from './candy-machine.service';
+import { HeliusService } from 'src/webhooks/helius/helius.service';
+
+@Module({
+  controllers: [CandyMachineController],
+  providers: [CandyMachineService,HeliusService],
+})
+export class CandyMachineModule {}
