160be1f1c30a4391bfefdd846931774e952760b7
Athar <athar.mohammad@dreader.io>
2023-03-02 19:18:04 +0530

    return empty array if no receipts

--- src/candy-machine/candy-machine.service.ts
+++ src/candy-machine/candy-machine.service.ts
@@ -466,19 +466,11 @@ export class CandyMachineService {
   }
 
   async findReceipts(query: CandyMachineReceiptParams) {
-    const receipts = await this.prisma.candyMachineReceipt.findMany({
+    return await this.prisma.candyMachineReceipt.findMany({
       skip: query.skip,
       take: query.take,
       where: { candyMachineAddress: query.candyMachineAddress },
     });
-
-    if (!receipts || !receipts.length) {
-      throw new NotFoundException(
-        `Receipts with Candy Machine address ${query.candyMachineAddress} does not exist`,
-      );
-    }
-
-    return receipts;
   }
 
   async generateMetaplexFileFromS3(key: string) {
