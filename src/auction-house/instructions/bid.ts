5d3d8a31f93e62259100f28853bd674184d6499c
Athar Mohammad <125286809+athar-d-reader@users.noreply.github.com>
2023-03-07 15:34:12 +0530

    feat: cancel a bid
    * feat: construct cancel bid transaction
    
    * add cancel-bid transaction endpoint
    
    * add withdrawFromBuyerEscrow Instruction
    
    * withdraw from buyer escrow on bid cancellation

--- src/auction-house/instructions/bid.ts
+++ src/auction-house/instructions/bid.ts
@@ -1,14 +1,21 @@
 import {
   AuctionHouse,
+  Bid,
   Metaplex,
+  NftWithToken,
+  Pda,
   PublicKey,
+  SftWithToken,
   SolAmount,
   SplTokenAmount,
   amount,
   lamports,
 } from '@metaplex-foundation/js';
 import {
+  CancelInstructionAccounts,
   createBuyInstruction,
+  createCancelBidReceiptInstruction,
+  createCancelInstruction,
   createPrintBidReceiptInstruction,
 } from '@metaplex-foundation/mpl-auction-house';
 import { createAssociatedTokenAccountInstruction } from '@solana/spl-token';
@@ -16,6 +23,7 @@ import {
   SYSVAR_INSTRUCTIONS_PUBKEY,
   TransactionInstruction,
 } from '@solana/web3.js';
+import { withdrawFromBuyerEscrow } from './withdrawFromBuyerEscrow';
 
 export const constructPrivateBidInstruction = async (
   metaplex: Metaplex,
@@ -137,3 +145,57 @@ export const constructPrivateBidInstruction = async (
 
   return instructions;
 };
+
+export function constructCancelBidInstruction(
+  metaplex: Metaplex,
+  bid: Bid,
+  auctionHouse: AuctionHouse,
+): TransactionInstruction[] {
+  const {
+    asset,
+    buyerAddress,
+    tradeStateAddress,
+    price,
+    receiptAddress,
+    tokens,
+  } = bid;
+
+  const {
+    authorityAddress,
+    address: auctionHouseAddress,
+    feeAccountAddress,
+  } = auctionHouse;
+
+  const tokenAccount = (asset as NftWithToken | SftWithToken).token.address;
+
+  const accounts: CancelInstructionAccounts = {
+    wallet: buyerAddress,
+    tokenAccount,
+    tokenMint: asset.address,
+    authority: authorityAddress,
+    auctionHouse: auctionHouseAddress,
+    auctionHouseFeeAccount: feeAccountAddress,
+    tradeState: tradeStateAddress,
+  };
+
+  const instruction: TransactionInstruction[] = [];
+  const args = {
+    buyerPrice: price.basisPoints,
+    tokenSize: tokens.basisPoints,
+  };
+
+  instruction.push(createCancelInstruction(accounts, args));
+  if (!!receiptAddress) {
+    instruction.push(
+      createCancelBidReceiptInstruction({
+        receipt: receiptAddress as Pda,
+        instruction: SYSVAR_INSTRUCTIONS_PUBKEY,
+      }),
+    );
+  }
+  instruction.push(
+    ...withdrawFromBuyerEscrow(metaplex, auctionHouse, buyerAddress, price),
+  );
+
+  return instruction;
+}
