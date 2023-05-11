49d42d3a3495fa017da90e310f396dc85c579fcd
Bartol Deak <b@bdeak.net>
2023-05-12 01:08:57 +0200

    mrsk.sh use dockerized version if not installed

--- config/scripts/mrsk.sh
+++ config/scripts/mrsk.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
 env=$1
-shift
 if [ -z "$env" ]; then
   echo "Usage: $0 <env> [mrsk args]"
   exit 1
 fi
+shift
 scripts=$(cd -P -- "$(dirname -- "$(realpath -- "$0")")" && pwd -P)
 
+bin="mrsk"
+if ! command -v "$bin"; then
+  bin="docker run -it --rm -v '$PWD:/workdir' -v '$SSH_AUTH_SOCK:/ssh-agent' -v /var/run/docker.sock:/var/run/docker.sock -e 'SSH_AUTH_SOCK=/ssh-agent' ghcr.io/mrsked/mrsk:latest"
+fi
+
 "$scripts/ssh-agent.sh" load "$env" 2>&1 | grep -v "^Identity added"
-sops exec-env "config/$env.enc.env" "mrsk $* -d $env"
+sops exec-env "config/$env.enc.env" "$bin $* -d $env"
 "$scripts/ssh-agent.sh" unload "$env" 2>&1 | grep -v "^Identity removed"
