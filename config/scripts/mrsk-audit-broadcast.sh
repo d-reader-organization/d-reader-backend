3f269a48e24529de29dc6df6349ecda3f8a11bee
Bartol Deak <b@bdeak.net>
2023-05-13 01:25:10 +0200

    setup grafana and basic dashboards

--- config/scripts/mrsk-audit-broadcast.sh
+++ config/scripts/mrsk-audit-broadcast.sh
@@ -4,4 +4,4 @@ command -v curl || apk add curl
 
 curl -X POST "$AUDIT_BROADCAST_WEBHOOK" \
   -H "Content-Type: application/json" \
-  -d "{ text: \"[d-reader] $*\" }"
+  -d "{ text: \"[d-reader-backend] $*\" }"
