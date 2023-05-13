884f9e6dbde34134b1a31ae540db4df1ba51ede4
Bartol Deak <b@bdeak.net>
2023-05-14 00:54:09 +0200

    add backend deploy github action

--- config/scripts/mrsk.sh
+++ config/scripts/mrsk.sh
@@ -8,12 +8,6 @@ fi
 shift
 scripts=$(cd -P -- "$(dirname -- "$(realpath -- "$0")")" && pwd -P)
 
-bin="mrsk"
-if ! command -v "$bin"; then
-  docker pull ghcr.io/mrsked/mrsk:latest
-  bin="docker run -it --rm -v '$PWD:/workdir' -v '$SSH_AUTH_SOCK:/ssh-agent' -v /var/run/docker.sock:/var/run/docker.sock -e 'SSH_AUTH_SOCK=/ssh-agent' ghcr.io/mrsked/mrsk:latest"
-fi
-
 quote() {
   _str=""
   for arg in "$@"; do
@@ -27,7 +21,5 @@ quote() {
 }
 
 "$scripts/ssh-agent.sh" load "$env" 2>&1 | grep -v "^Identity added"
-sops -d --output ".env.$env" "config/$env.enc.env"
-eval "$bin $(quote "$@") -d $env"
-rm -f ".env.$env"
+sops exec-env "config/$env.enc.env" "mrsk $(quote "$@") -d $env"
 "$scripts/ssh-agent.sh" unload "$env" 2>&1 | grep -v "^Identity removed"
