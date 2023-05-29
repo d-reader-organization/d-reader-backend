bb08abe89467e3454cbe559ed1496dd9aa7ba4a2
d-reader-josip <josip.volarevic@dreader.io>
2023-05-29 17:46:25 +0200

    chore: memoized throttle & debounce

---
+++ src/utils/lodash.ts
@@ -0,0 +1,55 @@
+import {
+  throttle,
+  memoize,
+  debounce,
+  ThrottleSettings,
+  DebounceSettings,
+  DebouncedFunc,
+} from 'lodash';
+
+export interface MemoizeDebouncedFunction<T extends (...args: any[]) => any> {
+  (...args: Parameters<T>): void;
+  flush: (...args: Parameters<T>) => void;
+}
+
+export function memoizeThrottle<T extends (...args: any[]) => any>(
+  func: T,
+  wait = 0,
+  options: ThrottleSettings = {},
+  resolver?: (...args: Parameters<T>) => any,
+): MemoizeDebouncedFunction<T> {
+  const mem = memoize<(...args: Parameters<T>) => DebouncedFunc<T>>(function (
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    ..._args: Parameters<T>
+  ) {
+    return throttle(func, wait, options);
+  }, resolver);
+
+  const wrappedFunction = (...args: Parameters<T>) => mem(...args)(...args);
+  wrappedFunction.flush = (...args: Parameters<T>): void => {
+    mem(...args).flush();
+  };
+
+  return wrappedFunction;
+}
+
+export function memoizeDebounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait = 0,
+  options: DebounceSettings = {},
+  resolver?: (...args: Parameters<T>) => any,
+): MemoizeDebouncedFunction<T> {
+  const mem = memoize<(...args: Parameters<T>) => DebouncedFunc<T>>(function (
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    ..._args: Parameters<T>
+  ) {
+    return debounce(func, wait, options);
+  }, resolver);
+
+  const wrappedFunction = (...args: Parameters<T>) => mem(...args)(...args);
+  wrappedFunction.flush = (...args: Parameters<T>): void => {
+    mem(...args).flush();
+  };
+
+  return wrappedFunction;
+}
