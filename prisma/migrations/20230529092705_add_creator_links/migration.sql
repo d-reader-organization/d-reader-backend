13517c05a90f47344b77c55462cd50f11bb6ceb2
d-reader-josip <josip.volarevic@dreader.io>
2023-05-29 12:54:18 +0200

    chore: add new Creator links

---
+++ prisma/migrations/20230529092705_add_creator_links/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "Creator" ADD COLUMN     "instagram" TEXT NOT NULL DEFAULT '',
+ADD COLUMN     "lynkfire" TEXT NOT NULL DEFAULT '',
+ADD COLUMN     "twitter" TEXT NOT NULL DEFAULT '';
