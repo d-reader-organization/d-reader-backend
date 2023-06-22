594712d140e4659bd7d9b1f36ad2b8aefa17534c
d-reader-josip <josip.volarevic@dreader.io>
2023-06-22 09:06:35 +0200

    chore: bump packages

--- src/candy-machine/candy-machine.service.ts
+++ src/candy-machine/candy-machine.service.ts
@@ -1,8 +1,4 @@
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import {
   Keypair,
   PublicKey,
