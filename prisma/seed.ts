c97df57072d04a558b4fde2f366d5515c4cf9144
d-reader-josip <josip.volarevic@dreader.io>
2023-07-08 14:17:00 +0200

    chore: rename comic.nameSubstring param

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -144,7 +144,7 @@ async function main() {
 
   console.log('✅ Emptied database!');
 
-  const skipS3Seed = true;
+  const skipS3Seed = false;
   if (!skipS3Seed) {
     console.log(`⛏️ Emptying '${s3.bucket}' s3 bucket...`);
     const keysToDelete = await s3.listFolderKeys({ Prefix: '' });
