36d08cb7d4374ccb2ebeed202b5b85f32d0d42ca
d-reader-luka <luka.crnogorac@dreader.io>
2023-08-08 21:02:46 +0200

    fix: user module providers dependecies

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,18 @@ import { UserController } from './user.controller';
 import { WalletService } from '../wallet/wallet.service';
 import { PasswordService } from '../auth/password.service';
 import { MailService } from '../mail/mail.service';
+import { HeliusService } from 'src/webhooks/helius/helius.service';
+import { WebSocketGateway } from 'src/websockets/websocket.gateway';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, WalletService, PasswordService, MailService],
+  providers: [
+    UserService,
+    WalletService,
+    PasswordService,
+    MailService,
+    HeliusService,
+    WebSocketGateway,
+  ],
 })
 export class UserModule {}
