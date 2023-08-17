c30a11b92f1c5221159505042227ea0ac90b8223
Mohd Athar <athar.mohammad@dreader.io>
2023-08-17 21:45:25 +0530

    support for different groups in mint-one endpoint

--- src/cli/mint-one-questions.ts
+++ src/cli/mint-one-questions.ts
@@ -1,5 +1,6 @@
 import { PublicKey } from '@solana/web3.js';
 import { QuestionSet, Question } from 'nest-commander';
+import { PUBLIC_GROUP_LABEL } from '../constants';
 
 @QuestionSet({ name: 'mint' })
 export class MintOneQuestions {
@@ -16,5 +17,20 @@ export class MintOneQuestions {
   })
   parseCandyMachineAddress(candyMachineAddress: string): PublicKey {
     return new PublicKey(candyMachineAddress);
+  }
+
+  @Question({
+    type: 'input',
+    name: 'label',
+    default: PUBLIC_GROUP_LABEL,
+    message: 'group label (defaults to public group)',
+    validate: async function (value: string) {
+      if (!value || value.length > 6 || value.length < 1)
+        return 'invalid group label';
+      return true;
+    },
+  })
+  parseLabel(label: string): string {
+    return label;
   }
 }
