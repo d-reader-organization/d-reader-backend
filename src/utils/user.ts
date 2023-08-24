dbd12dcf9e68e412a2f2e5fe8fffedcc2e3f48fb
d-reader-josip <josip.volarevic@dreader.io>
2023-08-24 11:48:57 +0200

    chore: change creator name validation

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -15,6 +15,16 @@ export function validateName(name: string) {
   }
 }
 
+export function validateCreatorName(name: string) {
+  if (typeof name !== 'string') {
+    throw new BadRequestException(`Bad name format: ${name || '<unknown>'}`);
+  } else if (!maxLength(name, USERNAME_MAX_SIZE)) {
+    throw new BadRequestException(`Max ${USERNAME_MAX_SIZE} characters`);
+  } else if (!minLength(name, USERNAME_MIN_SIZE)) {
+    throw new BadRequestException(`Min ${USERNAME_MIN_SIZE} characters`);
+  }
+}
+
 export function validateEmail(email: string) {
   if (typeof email !== 'string') {
     throw new BadRequestException(`Bad email format: ${email || '<unknown>'}`);
