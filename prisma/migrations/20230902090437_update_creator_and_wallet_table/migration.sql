baa3d4d47c6f225f34c4ad5a47bfba8c85b7b4c3
d-reader-josip <josip.volarevic@dreader.io>
2023-09-02 22:11:24 +0200

    fix: migrations

--- prisma/migrations/20230902090437_update_creator_and_wallet_table/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Creator" ADD COLUMN     "tippingAddress" TEXT NOT NULL DEFAULT '';
-
--- AlterTable
-ALTER TABLE "Wallet" ADD COLUMN     "label" TEXT NOT NULL DEFAULT '';
