d61a0eaa54a5910cc826e01c6af888dcd0697f8c
d-reader-josip <josip.volarevic@dreader.io>
2023-09-02 22:11:24 +0200

    fix: migrations

--- prisma/migrations/20230902190126_update_user_and_wallet_tables/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "deletedAt" TIMESTAMP(3);
-
--- AlterTable
-ALTER TABLE "Wallet" ADD COLUMN     "connectedAt" TIMESTAMP(3),
-ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
