64dfd1e78e617a5d81792ee003951c7ef65db87f
Athar Mohammad <125286809+athar-d-reader@users.noreply.github.com>
2023-09-05 16:44:15 +0530

    feat: settings module
    * feat:add global status model
    
    * feat: add settings module
    
    * chore: global status migrations
    
    * chore: settings module
    
    ---------
    
    Co-authored-by: d-reader-josip <josip.volarevic@dreader.io>

---
+++ src/settings/settings.controller.ts
@@ -0,0 +1,54 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { ThrottlerGuard } from '@nestjs/throttler';
+import { ApiTags } from '@nestjs/swagger';
+import {
+  GlobalStatusDto,
+  toGlobalStatus,
+  toGlobalStatusArray,
+} from './dto/global-status.dto';
+import { SettingsService } from './settings.service';
+import { CreateGlobalStatusDto } from './dto/create-global-status.dto';
+import { UpdateGlobalStatusDto } from './dto/update-global-status.dto';
+
+@UseGuards(ThrottlerGuard)
+@ApiTags('Settings')
+@Controller('settings')
+export class SettingsController {
+  constructor(private readonly settingService: SettingsService) {}
+
+  @Get('global-status')
+  async getGlobalStatus(): Promise<GlobalStatusDto[]> {
+    const globalStatus = await this.settingService.getGlobalStatus();
+    return toGlobalStatusArray(globalStatus);
+  }
+
+  @Post('global-status/create')
+  async createGlobalStatus(
+    @Body() createGlobalStatusDto: CreateGlobalStatusDto,
+  ): Promise<GlobalStatusDto> {
+    const globalStatus = await this.settingService.createGlobalStatus(
+      createGlobalStatusDto,
+    );
+    return toGlobalStatus(globalStatus);
+  }
+
+  @Patch('global-status/:id/update')
+  async updateGlobalStatus(
+    @Param('id') id: string,
+    @Body() updateGlobalStatusDto: UpdateGlobalStatusDto,
+  ) {
+    const globalStatus = await this.settingService.updateGlobalStatus(
+      +id,
+      updateGlobalStatusDto,
+    );
+    return toGlobalStatus(globalStatus);
+  }
+}
