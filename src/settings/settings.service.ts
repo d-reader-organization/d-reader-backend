34b1ecb8e76749cf1183401357feffbbb6df07ee
athar-d-reader <athar.mohammad@dreader.io>
2023-09-06 21:25:52 +0530

    feat: upload token icons to aws

--- src/settings/settings.service.ts
+++ src/settings/settings.service.ts
@@ -1,11 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'nestjs-prisma';
 import { CreateGlobalStatusDto } from './dto/create-global-status.dto';
 import { UpdateGlobalStatusDto } from './dto/update-global-status.dto';
+import { s3Service } from '../aws/s3.service';
 
 @Injectable()
 export class SettingsService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly s3: s3Service,
+  ) {}
 
   async getGlobalStatus() {
     return await this.prisma.globalStatus.findMany({
@@ -36,5 +40,27 @@ export class SettingsService {
     return await this.prisma.splToken.findMany({
       orderBy: { priority: 'asc' },
     });
+  }
+
+  async updateTokenIcon(id: number, file: Express.Multer.File) {
+    let token = await this.prisma.splToken.findUnique({ where: { id } });
+    const s3Folder = `spl-token/${token.name
+      .toLowerCase()
+      .replace(/\s+/g, '-')}/`;
+
+    const newFileKey = await this.s3.uploadFile(s3Folder, file, 'icon');
+    const oldFileKey = token.icon;
+    try {
+      token = await this.prisma.splToken.update({
+        where: { id },
+        data: { icon: newFileKey },
+      });
+    } catch {
+      await this.s3.deleteObject(newFileKey);
+      throw new BadRequestException('Malformed file upload');
+    }
+
+    await this.s3.garbageCollectOldFile(newFileKey, oldFileKey);
+    return token;
   }
 }
