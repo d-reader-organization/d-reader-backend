8030aa60f1a857a121f2c7e0ca5aff050e4ad5b7
athar-d-reader <athar.mohammad@dreader.io>
2023-09-25 16:13:27 +0530

    fix: fetch favourite domain

--- src/utils/sns.ts
+++ src/utils/sns.ts
@@ -1,11 +1,23 @@
 import { PublicKey } from '@solana/web3.js';
 import { getConnection } from './metaplex';
-import { getAllDomains, reverseLookup } from '@bonfida/spl-name-service';
+import {
+  getAllDomains,
+  getFavoriteDomain,
+  reverseLookup,
+} from '@bonfida/spl-name-service';
 
 export async function getOwnerDomain(wallet: PublicKey): Promise<string> {
   const connection = getConnection();
-  const domainKeys = await getAllDomains(connection, wallet);
-  if (!domainKeys.length) return undefined;
-  const domainName = await reverseLookup(connection, domainKeys[0]);
-  return domainName;
+  let domain: {
+    domain: PublicKey;
+    reverse: string;
+  };
+  try {
+    domain = await getFavoriteDomain(connection, wallet);
+  } catch (e) {
+    const domainKeys = await getAllDomains(connection, wallet);
+    if (!domainKeys.length) return;
+    return await reverseLookup(connection, domainKeys[0]);
+  }
+  return domain.reverse;
 }
