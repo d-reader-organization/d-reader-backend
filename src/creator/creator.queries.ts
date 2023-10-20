8d1ab1cbeed6298b1755f56b3012551934fc3834
d-reader-luka <luka.crnogorac@dreader.io>
2023-10-20 11:44:09 +0200

    fix creator raw query

--- src/creator/creator.queries.ts
+++ src/creator/creator.queries.ts
@@ -35,19 +35,27 @@ const getQueryFilters = (
 export const getCreatorsQuery = (query: CreatorFilterParams) => {
   const { nameCondition, sortColumn, sortOrder, filterCondition } =
     getQueryFilters(query);
-  return Prisma.sql`select creator.*, jsonb_agg(DISTINCT genre.*) FILTER (WHERE genre.slug IS NOT NULL) AS genres,
+  return Prisma.sql`select creator.*,
   SUM(case when userCreator."followedAt" is not null then 1 else 0 end)  as "followersCount"
   from "Creator" creator
-  left join "Comic" comic on comic."creatorId" = creator.id
-  left join "_ComicToGenre" "comicToGenre" on "comicToGenre"."A" = comic.slug
-  left join "Genre" genre on genre.slug = "comicToGenre"."B"  
   left join "UserCreator" userCreator on userCreator."creatorSlug" = creator.slug
   where creator."deletedAt" is null and creator."verifiedAt" is not null and creator."emailVerifiedAt" is not null
   ${filterCondition}
   ${nameCondition}
   group by creator.id
   ORDER BY ${sortColumn} ${sortOrder}
-  ${havingGenreSlugsCondition(query.genreSlugs)}
   OFFSET ${query.skip}
   LIMIT ${query.take};`;
+};
+
+export const getCreatorGenresQuery = (
+  creatorId: number,
+  genreSlugs?: string[],
+) => {
+  const filterCondition = Prisma.sql`where comic."creatorId" = ${creatorId}`;
+  return Prisma.sql`select jsonb_agg(DISTINCT genre.*) FILTER (WHERE genre.slug IS NOT NULL) AS genres from "Genre" genre 
+inner join "_ComicToGenre" ctg on genre.slug  = ctg."B" 
+inner join "Comic" comic on comic.slug = ctg."A" 
+${filterCondition}
+${havingGenreSlugsCondition(genreSlugs)};`;
 };
