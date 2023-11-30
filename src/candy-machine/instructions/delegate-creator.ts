7558a2fa36dcdae79f359247323c7419c74b6670
athar-d-reader <athar.mohammad@dreader.io>
2023-11-30 16:19:40 +0530

    feat: create delegate creator transaction

---
+++ src/candy-machine/instructions/delegate-creator.ts
@@ -0,0 +1,60 @@
+import { Metaplex, PublicKey } from '@metaplex-foundation/js';
+import { PUB_AUTH_TAG, pda } from './pda';
+import {
+  PROGRAM_ID as COMIC_VERSE_ID,
+  DelegateCreatorInstructionAccounts,
+  createDelegateCreatorInstruction,
+} from 'dreader-comic-verse';
+import { Transaction } from '@solana/web3.js';
+
+export async function constructDelegateCreatorInstruction(
+  candyMachineAddress: PublicKey,
+  collectionMint: PublicKey,
+  newCreator: PublicKey,
+  creatorAuthority: PublicKey,
+) {
+  const recordAuthority = pda(
+    [
+      Buffer.from(PUB_AUTH_TAG),
+      candyMachineAddress.toBuffer(),
+      collectionMint.toBuffer(),
+    ],
+    COMIC_VERSE_ID,
+  );
+
+  const accounts: DelegateCreatorInstructionAccounts = {
+    recordAuthority,
+    generator: candyMachineAddress,
+    collectionMint,
+    creatorAuthority,
+    newCreator,
+  };
+  return createDelegateCreatorInstruction(accounts);
+}
+
+export async function constructDelegateCreatorTransaction(
+  metaplex: Metaplex,
+  candyMachineAddress: PublicKey,
+  collectionMint: PublicKey,
+  newCreator: PublicKey,
+  creatorAuthority: PublicKey,
+) {
+  const instruction = await constructDelegateCreatorInstruction(
+    candyMachineAddress,
+    collectionMint,
+    newCreator,
+    creatorAuthority,
+  );
+
+  const latestBlockhash = await metaplex.connection.getLatestBlockhash();
+  const tx = new Transaction({
+    feePayer: creatorAuthority,
+    ...latestBlockhash,
+  }).add(instruction);
+
+  const rawTransaction = tx.serialize({
+    requireAllSignatures: false,
+    verifySignatures: false,
+  });
+  return rawTransaction.toString('base64');
+}
