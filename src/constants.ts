8ca69b3d93d0ee459636100581040759104b81f9
athar-d-reader <athar.mohammad@dreader.io>
2023-12-27 00:43:27 +0530

    fix: add lookuptable for devnet

--- src/constants.ts
+++ src/constants.ts
@@ -47,7 +47,9 @@ export const USERNAME_MAX_SIZE = 20;
 export const SAGA_COLLECTION_ADDRESS =
   '46pcSL5gmjBrPqGKFaLbbCmR6iVuLJbnQy13hAe7s6CC';
 export const AUCTION_HOUSE_LOOK_UP_TABLE = new PublicKey(
-  '9TzbC21XGK682N3eXntxV4cpSWVra2QzT4T8kb2m3AJj',
+  process.env.SOLANA_CLUSTER === 'mainnet'
+    ? '9TzbC21XGK682N3eXntxV4cpSWVra2QzT4T8kb2m3AJj'
+    : '9GfMG415sgpzGajfTZyt7qcDo6Xxoa97MyxTFuHuY7od',
 );
 export const USERNAME_VALIDATOR_REGEX = new RegExp(/^[a-zA-Z0-9-čćžšđČĆŽŠĐ]+$/);
 
