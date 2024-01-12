043f9a3aeb4297ff5c302453517d379725e856cb
Bartol Deak <b@bdeak.net>
2024-01-12 13:54:54 +0100

    implement throttler behind proxy

---
+++ src/guards/throttler-behind-proxy.guard.ts
@@ -0,0 +1,9 @@
+import { ThrottlerGuard } from '@nestjs/throttler';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ThrottlerBehindProxyGuard extends ThrottlerGuard {
+  protected getTracker(req: Record<string, any>): string {
+    return req.headers['cf-connecting-ip'] || req.ip;
+  }
+}
