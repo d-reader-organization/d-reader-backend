6d8d06d73e25986f0fb826687caf889ad7a333bb
atharmohammad <mohd.rule123@gmail.com>
2024-01-15 16:46:45 +0530

    fix: skip throttle for discord bot

--- src/guards/throttler-behind-proxy.guard.ts
+++ src/guards/throttler-behind-proxy.guard.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class ThrottlerBehindProxyGuard extends ThrottlerGuard {
   protected getTracker(req: Record<string, any>): string {
-    return req.headers['cf-connecting-ip'] || req.ip;
+    return req.headers ? req.headers['cf-connecting-ip'] : req.ip;
   }
 }
