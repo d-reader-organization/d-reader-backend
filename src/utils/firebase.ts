431315fa4f4ab1fc7776dab3237417c98a8cee82
d-reader-luka <luka.crnogorac@dreader.io>
2024-01-22 11:22:28 +0100

    store firebase creds in env value by value

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,16 +1,18 @@
 import admin from 'firebase-admin';
 
 export const initializeFirebase = () => {
-  const serviceAccountCreds: {
-    private_key: string;
-    client_email: string;
-    project_id: string;
-  } = JSON.parse(process.env.serviceAccount);
+  if (
+    !process.env.FIREBASE_PRIVATE_KEY ||
+    !process.env.FIREBASE_CLIENT_EMAIL ||
+    !process.env.FIREBASE_PROJECT_ID
+  ) {
+    throw Error('Missing Firebase credentials');
+  }
   admin.initializeApp({
     credential: admin.credential.cert({
-      clientEmail: serviceAccountCreds.client_email,
-      privateKey: serviceAccountCreds.private_key,
-      projectId: serviceAccountCreds.project_id,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      projectId: process.env.FIREBASE_PROJECT_ID,
     }),
   });
 };
