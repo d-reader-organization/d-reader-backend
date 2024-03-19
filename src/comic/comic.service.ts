a7c296337bc95b55df6418d13c6839907dbeb370
d-reader-luka <luka.crnogorac@dreader.io>
2024-03-19 14:34:37 +0100

    chore: favorite filter by bookmarkedAt

--- src/comic/comic.service.ts
+++ src/comic/comic.service.ts
@@ -178,7 +178,7 @@ export class ComicService {
       await this.prisma.userComic.findMany({
         where: {
           userId,
-          favouritedAt: {
+          bookmarkedAt: {
             not: null,
           },
         },
