465cf7254f27e7ba572318427e36568d8ca6a681
d-reader-luka <125265091+d-reader-luka@users.noreply.github.com>
2024-04-25 14:27:01 +0200

    chore: delete comicIssue.signature column

---
+++ prisma/migrations/20240425115859_delete_signature_column/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `signature` on the `ComicIssue` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ComicIssue" DROP COLUMN "signature";
