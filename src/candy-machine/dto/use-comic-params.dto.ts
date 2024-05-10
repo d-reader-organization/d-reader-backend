228af70e3c4d5c200c7813790715dd2d565b99c6
d-reader-luka <luka.crnogorac@dreader.io>
2024-05-10 11:48:56 +0200

    chore: add use comic issue asset endpoint

--- src/candy-machine/dto/use-comic-params.dto.ts
+++ src/candy-machine/dto/use-comic-params.dto.ts
@@ -1,8 +1,15 @@
+import { IsOptional } from 'class-validator';
 import { IsSolanaAddress } from '../../decorators/IsSolanaAddress';
 
 export class UseComicParams {
+  /* @deprecated */
   @IsSolanaAddress()
+  @IsOptional()
   nftAddress: string;
+
+  @IsSolanaAddress()
+  @IsOptional()
+  assetAddress: string;
 
   @IsSolanaAddress()
   ownerAddress: string;
