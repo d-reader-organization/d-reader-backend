fa71908fdd563812a5ea6571b26302ca234f311f
atharmohammad <mohd.rule123@gmail.com>
2024-05-22 12:49:21 +0530

    fix: tweet content

--- src/twitter/twitter.module.ts
+++ src/twitter/twitter.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TwitterService } from './twitter.service';
+import { TwitterController } from './twitter.controller';
 
 @Module({
+  controllers: [TwitterController],
   providers: [TwitterService],
 })
 export class TwitterModule {}
