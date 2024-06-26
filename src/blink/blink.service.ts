cbc6d5df973a8ef084671a0f126fd137f1e21f84
athar <athar.mohammad@dreader.io>
2024-06-26 22:49:33 +0530

    fix: check for slug params in mint action spec

--- src/blink/blink.service.ts
+++ src/blink/blink.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 import { ComicIssueService } from 'src/comic-issue/comic-issue.service';
 import { ActionSpecGetResponse } from './dto/types';
 import { s3Service } from 'src/aws/s3.service';
+import { Comic, ComicIssue, StatelessCover } from '@prisma/client';
+import { isNumberString } from 'class-validator';
 
 @Injectable()
 export class BlinkService {
@@ -12,17 +14,31 @@ export class BlinkService {
     private readonly s3: s3Service,
   ) {}
 
-  async getMintActionSpec(id: number): Promise<ActionSpecGetResponse> {
-    const { statelessCovers, comic, ...comicIssue } =
-      await this.prisma.comicIssue.findUnique({
-        where: { id },
+  async getMintActionSpec(id: string): Promise<ActionSpecGetResponse> {
+    const isIssueId = isNumberString(id);
+
+    let data: ComicIssue & { comic: Comic; statelessCovers: StatelessCover[] };
+    if (isIssueId) {
+      data = await this.prisma.comicIssue.findUnique({
+        where: { id: +id },
         include: { statelessCovers: true, comic: true },
       });
+    } else {
+      const key = id.split('_');
+      const [comicSlug, slug] = key;
+
+      data = await this.prisma.comicIssue.findFirst({
+        where: { comicSlug, slug },
+        include: { statelessCovers: true, comic: true },
+      });
+    }
+
+    const { comic, statelessCovers, ...comicIssue } = data;
     if (!comicIssue) {
       throw new Error('No such comic issue exists');
     }
     const activeCandyMachine =
-      await this.comicIssueService.findActiveCandyMachine(id);
+      await this.comicIssueService.findActiveCandyMachine(comicIssue.id);
 
     if (!activeCandyMachine) {
       throw new Error('No active mint for this issue');
