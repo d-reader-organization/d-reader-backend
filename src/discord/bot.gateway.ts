80673bc1352d8690f88c340b8ee6bf585c318dde
d-reader-luka <luka.crnogorac@dreader.io>
2024-08-19 11:00:16 +0200

    discord notifications

---
+++ src/discord/bot.gateway.ts
@@ -0,0 +1,119 @@
+import { EventParams, On } from '@discord-nestjs/core';
+import { Injectable, UseGuards } from '@nestjs/common';
+import { SkipThrottle } from '@nestjs/throttler';
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonInteraction,
+  ButtonStyle,
+  ClientEvents,
+  InteractionReplyOptions,
+  Message,
+  MessageActionRowComponentBuilder,
+} from 'discord.js';
+import { ComicIssueService } from 'src/comic-issue/comic-issue.service';
+import { ComicService } from 'src/comic/comic.service';
+import { CreatorService } from 'src/creator/creator.service';
+import { FounderRoleGuard } from 'src/guards/founder-role.guard';
+
+enum Action {
+  publish = 'publish',
+  verify = 'verify',
+}
+
+enum ButtonKey {
+  comic = 'comic',
+  comicIssue = 'comicIssue',
+  creator = 'creator',
+}
+
+@SkipThrottle()
+@Injectable()
+export class BotGateway {
+  constructor(
+    private readonly comicService: ComicService,
+    private readonly comicIssueService: ComicIssueService,
+    private readonly creatorService: CreatorService,
+  ) {}
+
+  @On('messageCreate')
+  async onMessage(message: Message): Promise<void> {
+    const embedsTitle = message.embeds?.at(0).title;
+    if (!embedsTitle) {
+      return;
+    }
+    const [key, value] = embedsTitle.split(':');
+
+    if (!Object.values<string>(ButtonKey).includes(key)) {
+      return;
+    }
+
+    const verify = new ButtonBuilder()
+      .setLabel('Verify')
+      .setStyle(ButtonStyle.Primary);
+
+    if (key === ButtonKey.creator) {
+      verify.setCustomId(`${Action.verify};${ButtonKey.creator};${value}`);
+      await message.reply({
+        components: [
+          new ActionRowBuilder<MessageActionRowComponentBuilder>({
+            components: [verify],
+          }),
+        ],
+      });
+      return;
+    }
+
+    const publish = new ButtonBuilder()
+      .setLabel('Publish')
+      .setStyle(ButtonStyle.Primary);
+
+    if (key === ButtonKey.comic) {
+      publish.setCustomId(`${Action.publish};${ButtonKey.comic};${value}`);
+      verify.setCustomId(`${Action.verify};${ButtonKey.comic};${value}`);
+    } else if (key === ButtonKey.comicIssue) {
+      publish.setCustomId(`${Action.publish};${ButtonKey.comicIssue};${value}`);
+      verify.setCustomId(`${Action.verify};${ButtonKey.comicIssue};${value}`);
+    }
+    const row = new ActionRowBuilder<MessageActionRowComponentBuilder>({
+      components: [publish, verify],
+    });
+    await message.reply({ components: [row] });
+  }
+
+  @On('interactionCreate')
+  @UseGuards(FounderRoleGuard)
+  async onButtonClicked(
+    @EventParams() eventArgs: ClientEvents['interactionCreate'],
+  ): Promise<InteractionReplyOptions> {
+    const buttonInteraction = eventArgs[0] as ButtonInteraction;
+    await buttonInteraction.deferReply({ ephemeral: true });
+    const [action, key, value] = buttonInteraction.customId.split(';');
+
+    const propertyName =
+      action === Action.publish ? 'publishedAt' : 'verifiedAt';
+    if (key === ButtonKey.comic) {
+      const message = (await this.comicService.toggleDatePropUpdate({
+        slug: value,
+        propertyName,
+        withMessage: true,
+      })) as string;
+      await buttonInteraction.followUp({ content: message });
+    } else if (key === ButtonKey.comicIssue) {
+      const message = (await this.comicIssueService.toggleDatePropUpdate({
+        id: +value,
+        propertyName,
+        withMessage: true,
+      })) as string;
+      await buttonInteraction.followUp({ content: message });
+    } else if (key === ButtonKey.creator) {
+      const message = (await this.creatorService.toggleDatePropUpdate({
+        slug: value,
+        propertyName: 'verifiedAt',
+        withMessage: true,
+      })) as string;
+      await buttonInteraction.followUp({ content: message });
+    }
+    return;
+  }
+}
--- src/discord/notification.service.ts
+++ src/discord/notification.service.ts
@@ -1,19 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import { Creator } from '@prisma/client';
+import { Comic, ComicIssue, Creator } from '@prisma/client';
 import config from '../configs/config';
 import { CREATOR_REGISTERED } from './templates/creatorRegistered';
 import { CREATOR_FILES_UPDATED } from './templates/creatorFilesUpdated';
 import { MessagePayload, WebhookClient } from 'discord.js';
 import { CreatorFile } from './dto/types';
+import { CREATOR_PROFILE_UPDATED } from './templates/creatorProfileUpdated';
+import { COMIC_UPDATED } from './templates/comicUpdated';
+import { COMIC_ISSUE_UPDATED } from './templates/comicIssueUpdated';
 
 @Injectable()
 export class DiscordNotificationService {
   private readonly apiUrl: string;
+  private readonly websiteUrl: string;
   private readonly payload: MessagePayload;
   private readonly discord?: WebhookClient;
 
   constructor() {
-    this.apiUrl = config().client.dPublisherUrl;
+    const clientConfig = config().client;
+    this.apiUrl = clientConfig.dPublisherUrl;
+    this.websiteUrl = clientConfig.dReaderUrl;
 
     if (!process.env.DISCORD_WEBHOOK_URL) {
       console.warn('DISCORD_WEBHOOK_URL is undefined');
@@ -57,6 +63,72 @@ export class DiscordNotificationService {
         "Error sending notification for creator's email verification",
         e,
       );
+    }
+  }
+
+  async notifyCreatorProfileUpdate({
+    oldCreator,
+    updatedCreator,
+  }: {
+    oldCreator: Creator;
+    updatedCreator: Creator;
+  }) {
+    try {
+      await this.discord?.send(
+        CREATOR_PROFILE_UPDATED({
+          oldData: oldCreator,
+          updatedData: updatedCreator,
+          websiteUrl: this.websiteUrl,
+          payload: this.payload,
+        }),
+      );
+    } catch (e) {
+      console.error(
+        "Error sending notification for creator's profile update",
+        e,
+      );
+    }
+  }
+
+  async notifyComicUpdated({
+    oldComic,
+    updatedComic,
+  }: {
+    oldComic: Comic;
+    updatedComic: Comic;
+  }) {
+    try {
+      await this.discord?.send(
+        COMIC_UPDATED({
+          oldData: oldComic,
+          updatedData: updatedComic,
+          websiteUrl: this.websiteUrl,
+          payload: this.payload,
+        }),
+      );
+    } catch (e) {
+      console.error('Error sending notification for comic updated', e);
+    }
+  }
+
+  async notifyComicIssueUpdated({
+    oldIssue,
+    updatedIssue,
+  }: {
+    oldIssue: ComicIssue;
+    updatedIssue: ComicIssue;
+  }) {
+    try {
+      await this.discord?.send(
+        COMIC_ISSUE_UPDATED({
+          oldData: oldIssue,
+          updatedData: updatedIssue,
+          websiteUrl: this.websiteUrl,
+          payload: this.payload,
+        }),
+      );
+    } catch (e) {
+      console.error('Error sending notification for comic updated', e);
     }
   }
 }
