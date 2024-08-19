91776fe35ac2ee834f75aa024aab2feb3feb31dd
d-reader-luka <luka.crnogorac@dreader.io>
2024-08-19 11:37:53 +0200

    hardcode value

--- src/guards/founder-role.guard.ts
+++ src/guards/founder-role.guard.ts
@@ -2,14 +2,15 @@ import { GuildMemberRoleManager, Interaction } from 'discord.js';
 import { CanActivate, ExecutionContext } from '@nestjs/common';
 import { InteractionType } from 'discord.js';
 
+/* TODO move to .env as DISCORD_FOUNDER_ROLE_ID */
+const founderRoleId = '1227650386094067772';
+
 export class FounderRoleGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const interaction = context.getArgByIndex(0) as Interaction;
     if (interaction.type === InteractionType.MessageComponent) {
       const roleManager = interaction.member.roles as GuildMemberRoleManager;
-      const modRole = await interaction.guild.roles.fetch(
-        process.env.DISCORD_FOUNDER_ROLE_ID,
-      );
+      const modRole = await interaction.guild.roles.fetch(founderRoleId);
       return roleManager.member.roles.highest.position >= modRole.position;
     }
   }
