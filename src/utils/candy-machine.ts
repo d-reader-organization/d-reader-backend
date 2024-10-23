8f988ff0293a7c842be0b78606073cadb23da97f
athar <athar.mohammad@dreader.io>
2024-10-23 16:36:13 +0530

    fix: isSolPayment flag

--- src/utils/candy-machine.ts
+++ src/utils/candy-machine.ts
@@ -381,7 +381,7 @@ export function validateBalanceForMint(
   const totalMintPrice = numberOfItems * mintPrice;
   const totalProtocolFeeRequired = numberOfItems * protocolFee;
 
-  const isSolPayment = tokenBalance > 0;
+  const isSolPayment = tokenBalance == 0;
 
   const missingSolFunds = isSolPayment
     ? totalMintPrice + totalProtocolFeeRequired - solBalance
