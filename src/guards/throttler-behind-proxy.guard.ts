b09ea417c3f8b90648809d1be7d82906ab236159
d-reader-josip <josip.volarevic@dreader.io>
2024-11-15 20:12:13 +0100

    chore: add logs

--- src/guards/throttler-behind-proxy.guard.ts
+++ src/guards/throttler-behind-proxy.guard.ts
@@ -1,12 +1,15 @@
 import { ThrottlerGuard } from '@nestjs/throttler';
 import { Injectable } from '@nestjs/common';
 import { getClientIp } from 'request-ip';
+import { Request } from 'express';
 
 @Injectable()
 export class ThrottlerBehindProxyGuard extends ThrottlerGuard {
-  protected async getTracker(req: Record<string, any>): Promise<string> {
+  protected async getTracker(req: Request): Promise<string> {
+    console.log('user agent: ', req['User-Agent']);
     const clientIp = getClientIp(req);
     console.log(`DEBUG: throttler ip: ${clientIp}`);
+    console.log(`DEBUG: headers: ${req.headers}`);
     return clientIp;
   }
 }
