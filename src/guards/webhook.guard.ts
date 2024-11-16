c536a157bd2a254737d557755ad400c01821d7fa
d-reader-josip <josip.volarevic@dreader.io>
2024-11-16 17:45:17 +0100

    fix: webhook guard

--- src/guards/webhook.guard.ts
+++ src/guards/webhook.guard.ts
@@ -6,7 +6,7 @@ export class WebhookGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const { headers } = context.switchToHttp().getRequest();
 
-    if (!headers.authoriziration) return false;
+    // if (!headers.authorization) return false;
     const [, token] = headers.authorization.split('Bearer ');
 
     const { webhook } = jwt.verify(token, process.env.JWT_ACCESS_SECRET) as {
