0682cf543f1ceb09894778ff4c3cddbe8174ae6c
d-reader-josip <josip.volarevic@dreader.io>
2024-11-16 16:31:45 +0100

    chore: cache interceptor cleanups

--- src/interceptors/cache.module.ts
+++
@@ -1,9 +0,0 @@
-import { CacheModule as CacheManagerModule } from '@nestjs/cache-manager';
-import { Module, Global } from '@nestjs/common';
-
-@Global()
-@Module({
-  imports: [CacheManagerModule.register()],
-  exports: [CacheManagerModule.register()],
-})
-export class CacheModule {}
