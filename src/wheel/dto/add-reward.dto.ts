d0fc309293742196bf0f705ed61904bf6a0453a3
athar <athar.mohammad@dreader.io>
2024-12-09 16:20:07 +0530

    chore: handle different rewards

--- src/wheel/dto/add-reward.dto.ts
+++ src/wheel/dto/add-reward.dto.ts
@@ -1,23 +1,11 @@
-import { Transform } from 'class-transformer';
-import {
-  IsEnum,
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-  IsString,
-  MaxLength,
-} from 'class-validator';
+import { IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
 import { WheelRewardType } from '@prisma/client';
-import { ApiProperty, IntersectionType } from '@nestjs/swagger';
+import { TransformNumberToString } from 'src/utils/transform';
 
-export class AddRewardBodyDto {
-  @IsNotEmpty()
-  @MaxLength(48)
-  name: string;
-
-  @IsOptional()
-  @MaxLength(1024)
-  description?: string;
+export class AddRewardDto {
+  @TransformNumberToString()
+  @IsString()
+  typeId: string;
 
   @IsEnum(WheelRewardType)
   type: WheelRewardType;
@@ -30,23 +18,8 @@ export class AddRewardBodyDto {
 
   @IsOptional()
   @IsString()
-  assetAddress?: string;
-
-  @IsOptional()
-  @IsString()
   externalLink?: string;
 
   @IsNumber()
   winProbability: number;
 }
-
-export class AddRewardFilesDto {
-  @ApiProperty({ type: 'string', format: 'binary' })
-  @Transform(({ value }) => value[0])
-  image: Express.Multer.File | null;
-}
-
-export class AddRewardDto extends IntersectionType(
-  AddRewardBodyDto,
-  AddRewardFilesDto,
-) {}
