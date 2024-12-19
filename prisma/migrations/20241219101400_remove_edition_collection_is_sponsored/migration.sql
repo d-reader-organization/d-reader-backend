9e7c3549628193aee9520e68f57185cac34915d0
atharmohammad <mohd.rule123@gmail.com>
2024-12-19 15:47:22 +0530

    chore: remove PrintEditionCollection isSponsored Column

---
+++ prisma/migrations/20241219101400_remove_edition_collection_is_sponsored/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isSponsored` on the `PrintEditionCollection` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "PrintEditionCollection" DROP COLUMN "isSponsored";
