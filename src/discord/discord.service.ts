264ddde1733c9edc78b5099b019d76a3b8c14586
atharmohammad <mohd.rule123@gmail.com>
2025-01-14 18:22:05 +0530

    chore: throttle request autograph endpoint

--- src/discord/discord.service.ts
+++ src/discord/discord.service.ts
@@ -27,7 +27,6 @@ import { ERROR_MESSAGES } from 'src/utils/errors';
 import { RequestSignatureMessagePayload } from './templates/requestSignatureMessagePayload';
 import { getPublicUrl } from '../aws/s3client';
 import { getDiscordUser } from 'src/utils/discord';
-import { UserPayload } from 'src/auth/dto/authorization.dto';
 
 const logError = (notificationType: string, e: any) => {
   console.error(`Failed to send notification for ${notificationType}`);
@@ -46,13 +45,13 @@ export class DiscordService {
       return;
     }
 
-    if (!process.env.AUTOGRAPH_WEBHOOK_URL) {
+    if (!process.env.DISCORD_AUTOGRAPH_WEBHOOK_URL) {
       console.warn('AUTOGRAPH_WEBHOOK_URL is undefined');
       return;
     }
 
     this.autographWebhook = new WebhookClient({
-      url: process.env.AUTOGRAPH_WEBHOOK_URL,
+      url: process.env.DISCORD_AUTOGRAPH_WEBHOOK_URL,
     });
     this.discord = new WebhookClient({ url: process.env.DISCORD_WEBHOOK_URL });
     this.payload = new MessagePayload(
@@ -61,7 +60,7 @@ export class DiscordService {
     );
   }
 
-  async requestAutograph(user: UserPayload, address: string) {
+  async requestAutograph(username: string, address: string) {
     const asset = await this.prisma.collectibleComic.findUnique({
       where: { address },
       include: {
@@ -78,6 +77,10 @@ export class DiscordService {
         },
       },
     });
+
+    if (!asset) {
+      throw new BadRequestException(ERROR_MESSAGES.ASSET_NOT_FOUND(address));
+    }
 
     const creator = await this.prisma.creator.findFirst({
       where: {
@@ -118,7 +121,7 @@ export class DiscordService {
 
     const buttonComponent: APIButtonComponentWithCustomId = {
       label: `Sign comic ✍🏼`,
-      custom_id: `${user.username};${address}}`,
+      custom_id: `${username};${address}}`,
       style: ButtonStyle.Success,
       type: ComponentType.Button,
     };
@@ -139,7 +142,7 @@ export class DiscordService {
     await this.autographWebhook.send(
       RequestSignatureMessagePayload({
         payload: this.payload,
-        content: `**${user.username}** requested **${creatorUsername}** to sign their **${asset.name}**`,
+        content: `**${username}** requested **${creatorUsername}** to sign their **${asset.name}**`,
         imageUrl: getPublicUrl(cover.image),
         nftName: asset.name,
         rarity,
