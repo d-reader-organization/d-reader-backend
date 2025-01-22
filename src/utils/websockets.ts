cafb6cabd4dbe842a464aee4ab8062f982cbc4cc
d-reader-luka <luka.crnogorac@dreader.io>
2025-01-22 13:49:38 +0100

    chore: rename event

--- src/utils/websockets.ts
+++ src/utils/websockets.ts
@@ -1,3 +1,3 @@
 export const DAILY_DROPS_ROOM_ID = 'daily-drops';
 
-export const DAILY_DROP_REWARD_EVENT_NAME = 'daily-drop-reward';
+export const DAILY_DROP_WINNER_ANNOUNCEMENT = 'daily-drop-winner-announcement';
