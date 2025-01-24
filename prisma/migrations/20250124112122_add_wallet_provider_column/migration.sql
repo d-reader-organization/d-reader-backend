1245e2f8efe0316b484a0958c87f922b55f19467
d-reader-luka <luka.crnogorac@dreader.io>
2025-01-24 12:23:46 +0100

    chore: update enum value

--- prisma/migrations/20250124112122_add_wallet_provider_column/migration.sql
+++ prisma/migrations/20250124112122_add_wallet_provider_column/migration.sql
@@ -1,5 +1,5 @@
 -- CreateEnum
-CREATE TYPE "WalletProvider" AS ENUM ('NonCustodial', 'Privy');
+CREATE TYPE "WalletProvider" AS ENUM ('NonCustodial', 'Embedded');
 
 -- AlterTable
 ALTER TABLE "Wallet" ADD COLUMN     "provider" "WalletProvider" NOT NULL DEFAULT 'NonCustodial';
