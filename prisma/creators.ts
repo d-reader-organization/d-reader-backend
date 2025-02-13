4cf5cc1a9fdd2383d82ec4a2a2930b27aefc2658
atharmohammad <mohd.rule123@gmail.com>
2025-02-13 12:47:00 +0530

    fix: creator seed

--- prisma/creators.ts
+++ prisma/creators.ts
@@ -16,12 +16,12 @@ export const studioNx = async (): Promise<Prisma.UserCreateArgs['data']> => ({
   emailVerifiedAt: new Date(),
   channel: {
     create: {
-      handle: 'StudioNX',
+      handle: 'studionx',
+      displayName: 'StudioNX',
       avatar: 'creators/studio-nx/avatar.png',
       banner: 'creators/studio-nx/banner.jpg',
       description:
         'StudioNX is an Emmy award winning visual development house that creates character driven IP for feature film, TV & games.',
-      flavorText: 'Look at that, we have an Emmy award!',
       website: 'https://studionx.com',
       twitter: 'https://twitter.com/StudioNX',
       instagram: 'https://www.instagram.com/jim_bryson',
@@ -40,12 +40,12 @@ export const swamplabs = async (): Promise<Prisma.UserCreateArgs['data']> => ({
   emailVerifiedAt: new Date(),
   channel: {
     create: {
-      handle: 'Swamplabs',
+      handle: 'swamplabs',
+      displayName: 'Swamplabs',
       avatar: 'creators/swamplabs/avatar.png',
       banner: 'creators/swamplabs/banner.jpg',
       description:
         'Swamplabs is a studio that creates comics and mangas by latest standards, while paying the artists for the cheapest possible amount',
-      flavorText: 'Lorem Ipsum dolor sit flavor text',
       website: 'https://swamplabs.com',
       twitter: 'https://twitter.com/lupers_world',
       verifiedAt: new Date(),
@@ -65,12 +65,12 @@ export const gonneytoons = async (): Promise<
   role: 'Creator',
   channel: {
     create: {
-      handle: 'Gooneytoons',
+      handle: 'gooneytoons',
+      displayName: 'Gooneytoons',
       avatar: 'creators/gooneytoons-studio/avatar.png',
       banner: 'creators/gooneytoons-studio/banner.png',
       description:
         'Gooneytoons is a creative studio that breathes life into captivating comics and mesmerizing illustrations, fueling imagination with every stroke of the pen.',
-      flavorText: '“Such nasty little creatures” - My dad',
       website: 'https://gooneytoons.studio',
       twitter: 'https://twitter.com/GooneyToonsNFT',
       instagram: 'https://www.instagram.com/gooneytoons.nft',
@@ -89,13 +89,12 @@ export const saucerpen = async (): Promise<Prisma.UserCreateArgs['data']> => ({
   role: 'Creator',
   channel: {
     create: {
-      handle: 'Saucerpen',
+      handle: 'saucerpen',
+      displayName: 'Saucerpen',
       avatar: 'creators/saucerpen/avatar.jpg',
       banner: 'creators/saucerpen/banner.jpg',
       description:
         'Hello! I am an illustrator, comic artist and graphic designer from Rijeka, Croatia',
-      flavorText:
-        '“Amazing artist & illustrator” - Croatian Academy of Fine Arts',
       website: 'https://korinahunjak.com/',
       instagram: 'https://www.instagram.com/korina.hunjak/',
       verifiedAt: new Date(),
@@ -113,12 +112,12 @@ export const madMuse = async (): Promise<Prisma.UserCreateArgs['data']> => ({
   emailVerifiedAt: new Date(),
   channel: {
     create: {
-      handle: 'MadMuse',
+      handle: 'madmuse',
+      displayName: 'MadMuse',
       avatar: 'creators/mad-muse-syndicate/avatar.jpg',
       banner: 'creators/mad-muse-syndicate/banner.jpg',
       description:
         'A storytelling studio by Roach Writes, bringing worlds to life while crafting character driven stories in web3',
-      flavorText: 'The muse pulls the strings. We are just her puppets.',
       website: 'https://www.jameseroche.com',
       twitter: 'https://twitter.com/RoachWrites_',
       instagram: 'https://www.instagram.com/jameseroche',
@@ -137,12 +136,12 @@ export const tsukiverse = async (): Promise<Prisma.UserCreateArgs['data']> => ({
   emailVerifiedAt: new Date(),
   channel: {
     create: {
-      handle: 'Goose0x',
+      handle: 'goose0x',
+      displayName: 'Goose0x',
       avatar: 'creators/goose-0-x/avatar.jpg',
       banner: 'creators/goose-0-x/banner.jpg',
       description:
         'We are an Art first, story-driven project, with a unique nostalgic feel to make the collection and our brand stand out - all with your support as a loving community',
-      flavorText: 'brought to life by: @fake.com',
       website: 'https://www.tsukiverse.io',
       twitter: 'https://twitter.com/tsukiversenft',
       instagram: 'https://www.instagram.com/tsukiv3rse/',
@@ -162,11 +161,11 @@ export const longwood = async (): Promise<Prisma.UserCreateArgs['data']> => ({
   emailVerifiedAt: new Date(),
   channel: {
     create: {
-      handle: 'LongwoodLabs',
+      handle: 'longwoodlabs',
+      displayName: 'LongwoodLabs',
       avatar: 'creators/longwood-labs/avatar.jpg',
       banner: 'creators/longwood-labs/banner.jpg',
       description: 'Web3 idle gaming studio | Creators of @fake.com',
-      flavorText: 'The best gaming studio in web3',
       website: 'https://linktr.ee/theheistgame',
       twitter: 'https://twitter.com/playtheheist',
       verifiedAt: new Date(),
